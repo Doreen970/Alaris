e9f5efbf8ac2f77b3e4d397f0abe568006dd0c95
Doreen970 <doreenwanyama20@gmail.com>
2023-06-11 15:05:20 +0300

    Worked on search feature by adding query filter capabilities Added categories to sidebar menu Added clear filter features

--- item/views.py
+++ item/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from .models import Item, Category
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm
+from django.db.models import Q
 
 # Create your views here.
 
 def items(request):
     items = Item.objects.filter(is_sold=False)
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    query = request.GET.get('query', '')
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(category__name__icontains=query))
     
     return render(request, 'item/items.html', {
         'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
     })
 
 def detail(request, pk):
